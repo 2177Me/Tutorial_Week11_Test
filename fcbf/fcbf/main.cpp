2498834c2e54132f59bf5614fabadb42a741d6c8
2177Me <ac1811409@akfauniversity.org>
2022-04-28 12:56:03 +0500

    xcode proj is added

---
+++ fcbf/fcbf/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fcbf
+//
+//  Created by MEKHRIBON JABBOROVA on 28/04/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
